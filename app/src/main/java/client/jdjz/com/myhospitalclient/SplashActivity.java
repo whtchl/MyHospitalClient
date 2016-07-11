19620483e5a4903500983048c98d17e718bb3344
tchl <whtchl@126.com>
2016-07-11 16:54:05 +0800

    init project

---
+++ app/src/main/java/client/jdjz/com/myhospitalclient/SplashActivity.java
@@ -0,0 +1,14 @@
+package client.jdjz.com.myhospitalclient;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
